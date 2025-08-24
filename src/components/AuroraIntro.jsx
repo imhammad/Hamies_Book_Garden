85de202b182ab74d7500f267a622ef955c7fa64b
Hammad Hassan <hammadhassan319@gmail.com>
2025-08-24 16:31:17 +0800

    aurora_modified2

--- src/components/AuroraIntro.jsx
+++ src/components/AuroraIntro.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion'
+
+
+const colors = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 const AuroraIntro = () => {
+
+    const color = useMotionValue(colors[0])
+    const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color} )`
   return (
-    <section className='relative grid min-h-screen place-content-center overflow-hidden bg-gray-950
+    <motion.section 
+    style={{
+        backgroundImage,
+    }}
+    className='relative grid min-h-screen place-content-center overflow-hidden bg-gray-950
     px-4 py-24 text-gray-200'>
 
-    </section>
+    </motion.section>
   )
 }
 
