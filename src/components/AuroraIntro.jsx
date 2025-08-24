c8d6e719701bee88b22751b058476c9da8112b94
Hammad Hassan <hammadhassan319@gmail.com>
2025-08-24 17:00:49 +0800

    aurora_section_modified

--- src/components/AuroraIntro.jsx
+++ src/components/AuroraIntro.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { FiArrowRight } from "react-icons/fi";
 
 import { motion, useMotionTemplate, useMotionValue, animate } from 'framer-motion'
 
@@ -8,6 +9,9 @@ const AuroraIntro = () => {
 
     const color = useMotionValue(colors[0])
     const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color} )`
+
+    const border = useMotionTemplate`1px solid ${color}`;
+    const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
 
     useEffect(() => {
         animate(color, colors, {
@@ -41,6 +45,24 @@ const AuroraIntro = () => {
                 The story's beating heart, the reader's final word.
                 Your next literary journey begins here.
             </p>
+
+            <motion.button
+                style={{
+                    border,
+                    boxShadow,
+                }}
+                whileHover={{
+                    scale: 1.015,
+                }}
+                whileTap={{
+                    scale: 0.985,
+                }}
+                className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10
+                px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+                >
+                Start reviewing now
+                <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+            </motion.button>
 
         </div>
 
